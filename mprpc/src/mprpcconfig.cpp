97594ab5a5256d289898d78e4c9559a4fb93cd22
yzfzzz <20zfye@stu.edu.cn>
2024-12-22 19:22:24 +0800

    feat: 增加域名解析功能并更新配置
    - 在 MprpcConfig 类中添加域名解析功能，支持解析 rpcserverip 和 zookeeperip
    - 更新 address.conf 文件，将 IP 地址改为域名 yzfzzz-gmpanel.online
    - 优化代码格式，调整缩进和空格

--- mprpc/src/mprpcconfig.cpp
+++ mprpc/src/mprpcconfig.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <string>
+#include <unordered_map>
 #include "mprpcconfig.h"
 //  复杂解析加载配置文件
-void MprpcConfig::LoadConfigFile(const char* config_file) {
-    FILE* pf = fopen(config_file, "r");
+void MprpcConfig::LoadConfigFile(const char *config_file) {
+    FILE *pf = fopen(config_file, "r");
     if (pf == nullptr) {
         std::cout << config_file << "is not exist" << std::endl;
         exit(EXIT_FAILURE);
@@ -35,11 +43,53 @@ void MprpcConfig::LoadConfigFile(const char* config_file) {
         int endidx = read_buf.find('\n', idx);
         value = read_buf.substr(idx + 1, endidx - idx - 1);
         Trim(value);
+
+        // 域名解析
+        if (key == "rpcserverip" || key == "zookeeperip") {
+            std::unordered_map<std::string, std::string> ip_map;
+            struct addrinfo hints, *result, *rp;
+            int status = -1;
+            std::string hostname = value;  // 要解析的域名
+
+            memset(&hints, 0, sizeof(struct addrinfo));
+            hints.ai_family = AF_UNSPEC;      // 允许IPv4和IPv6
+            hints.ai_socktype = SOCK_STREAM;  // 用于TCP连接
+
+            status = getaddrinfo(hostname.c_str(), NULL, &hints, &result);
+            if (status != 0) {
+                std::cerr << "getaddrinfo error: " << gai_strerror(status)
+                          << std::endl;
+                return;
+            }
+
+            for (rp = result; rp != NULL; rp = rp->ai_next) {
+                char ipstr[INET6_ADDRSTRLEN];
+                void *addr;
+                if (rp->ai_family == AF_INET) {
+                    // IPv4地址
+                    struct sockaddr_in *ipv4 =
+                        (struct sockaddr_in *)rp->ai_addr;
+                    addr = &(ipv4->sin_addr);
+                    inet_ntop(rp->ai_family, addr, ipstr, sizeof(ipstr));
+                    ip_map["IPV4"] = ipstr;
+                } else {
+                    // IPv6地址
+                    struct sockaddr_in6 *ipv6 =
+                        (struct sockaddr_in6 *)rp->ai_addr;
+                    addr = &(ipv6->sin6_addr);
+                    inet_ntop(rp->ai_family, addr, ipstr, sizeof(ipstr));
+                    ip_map["IPV6"] = ipstr;
+                }
+            }
+            freeaddrinfo(result);
+            value = ip_map["IPV4"];
+        }
+
         m_configMap.insert(std::pair<std::string, std::string>(key, value));
     }
 }
 // 查询配置项信息
-std::string MprpcConfig::Load(const std::string& key) {
+std::string MprpcConfig::Load(const std::string &key) {
     auto it = m_configMap.find(key);
     if (it == m_configMap.end()) {
         return "";
@@ -48,7 +98,7 @@ std::string MprpcConfig::Load(const std::string& key) {
 }
 
 // 去掉字符串前后的空格
-void MprpcConfig::Trim(std::string& src_buf) {
+void MprpcConfig::Trim(std::string &src_buf) {
     // 找到第一个不为空格的索引
     int idx = src_buf.find_first_not_of(' ');
     if (idx != -1) {
